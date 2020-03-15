6a8650c62e24c1d7e42ec6af1776aaecb7421a38
Lic <Lic_plus@163.com>
2020-03-15 21:04:37 +0800

    py脚本小工具

--- send_ip.py
+++ send_ip.py
@@ -12,7 +12,7 @@ from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
 #树莓派开机发ip到邮箱
 #发送邮件的基本函数，参数依次如下
-# smtp服务器地址、邮箱用户名，邮箱秘密，发件人地址，手贱儿女地址（列表的方式），邮件主题，邮件html内容
+# smtp服务器地址、邮箱用户名，邮箱token_key，发件人地址，接收人地址（列表的方式），邮件主题，邮件html内容
 def sendEmail(smtpserver,username,password,sender,receiver,subject,msghtml):
     msgRoot = MIMEMultipart('related')
     msgRoot["To"] = ','.join(receiver)
