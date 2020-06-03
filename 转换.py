e2b92bbb6f4a7ac271508b6cd121899155cdadc9
Lic <Lic_plus@163.com>
2020-06-03 13:11:46 +0800

    py脚本小工具

--- 转换.py
+++ 转换.py
@@ -12,6 +12,7 @@ def get_path():
                      if filename.endswith((".doc", ".docx"))]
     for wordname in wordname_list:
         # 分离word文件名称和后缀，转化为pdf名称
+        print("正在转换"+wordname)
         pdfname = os.path.splitext(wordname)[0] + '.pdf'
         # 如果当前word文件对应的pdf文件存在，则不转化
         if pdfname in filename_list:
@@ -24,18 +25,22 @@ def get_path():
 
 
 def convert_word_to_pdf():
+    # word转化为pdf
     word = comtypes.client.CreateObject("Word.Application")
     word.Visible = 0
     for wordpath,pdfpath in get_path():
         newpdf = word.Documents.Open(wordpath)
         newpdf.SaveAs(pdfpath, FileFormat=17)
         newpdf.Close()
+
+def convert_ppt_to_pdf():
     # ppt转化为pdf
-    # ppt = comtypes.client.CreateObject("Powerpoint.Application")
-    # ppt.Visible = 1
-    # newpdf = ppt.Presentations.Open(in_file)
-    # newpdf.SaveAs(out_file, FileFormat=32)
-    # newpdf.Close()
+    ppt = comtypes.client.CreateObject("Powerpoint.Application")
+    ppt.Visible = 1
+    for ppt_file, ppt_file in get_path():
+        newpdf = ppt.Presentations.Open(ppt_file)
+        newpdf.SaveAs(ppt_file, FileFormat=32)
+        newpdf.Close()
 
 
 if __name__ == "__main__":
