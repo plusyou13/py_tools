bd3df926b67eb71cccebf247d45d02cd93dd1c69
li_c8 <li_c8@ecidi>
2020-12-09 15:06:17 +0800

    tools

--- acwing/python版本/generate-parentheses.py
+++ acwing/python版本/generate-parentheses.py
@@ -1,5 +1,25 @@
+'''
+22. 括号生成
+数字 n 代表生成括号的对数，请你设计一个函数，用于能够生成所有可能的并且 有效的 括号组合。
+
+示例：
+
+输入：n = 3
+输出：[
+       "((()))",
+       "(()())",
+       "(())()",
+       "()(())",
+       "()()()"
+     ]
+'''
+
+from typing import List
+
 
 class Solution:
+
+
     def generateParenthesis(self, n: int) -> List[str]:
         if n == 0:
             return []
@@ -23,5 +43,5 @@ class Solution:
         return total_l[n]
 
 solution= Solution()
-num=solution.generateParenthesis(1)
+num=solution.generateParenthesis(9)
 print(num)
